813737724e2a778887b58b66b5b75973dca3de57
Aman kumar Jha <68696279+aman8198@users.noreply.github.com>
2021-11-22 09:59:54 +0530

    Animal abstraction
    data abstraction

---
+++ Animalbstract/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Animalbstract
+{
+    abstract class Animal
+    {
+        public abstract void Animaleatinghabit();
+
+
+    }
+    class Herbivores : Animal
+    {
+        public override void Animaleatinghabit()
+        {
+            Console.WriteLine("Eats only vegetable");
+        }
+        public void Demo()
+        {
+            Console.WriteLine(".............");
+        }
+    }
+    class Carnivores : Animal
+    {
+        public override void Animaleatinghabit()
+        {
+            Console.WriteLine("Eats other Animal");
+        }
+    }
+
+    class Program
+    {
+      
+        public void CallFun(Animal a)
+        {
+            if(a.GetType() == typeof(Herbivores))
+            {
+                Herbivores H = (Herbivores)a;
+                H.Demo();
+            }
+            a.Animaleatinghabit();
+        }
+        static void Main(string[] args)
+        {
+
+            Herbivores h = new Herbivores();
+            Carnivores c = new Carnivores();
+            Animal h1 = new Herbivores();
+            h.Animaleatinghabit();
+            c.Animaleatinghabit();
+            Program p = new Program();
+            p.CallFun(h1);
+         
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
