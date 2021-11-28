7b9890fb72baeb116607fcbc72e1e7c443c020bc
Aman kumar Jha <68696279+aman8198@users.noreply.github.com>
2021-11-28 11:45:11 +0530

    Assignment 7
    Demo Registration form with validation and login form implementation in asp.net

---
+++ DemoLogin/LoginForm_demo.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace DemoLogin
+{
+    public partial class LoginForm_demo : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Server=.;Database=Emp_db;trusted_connection=true");
+            SqlCommand cmd = new SqlCommand("select * from Signup_demo where Mail=@Mail and Password=@Password", con);
+            cmd.Parameters.AddWithValue("@Mail", TextBox1.Text);
+            cmd.Parameters.AddWithValue("@Password", TextBox2.Text);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if(dt.Rows.Count > 0)
+            {
+                Response.Redirect("RedirectForm.aspx");
+            }
+            else
+            {
+                Label1.Text = "Invalid usernname or password";
+                Label1.ForeColor = System.Drawing.Color.Red;
+
+            }
+
+
+
+        }
+    }
+}
---
+++ DemoLogin/SignUp.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DemoLogin
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Server=.;Database=Emp_db;trusted_connection=true");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Signup_demo values(@Name, @Password, @City, @Gender, @Mail)", con);
+            cmd.Parameters.AddWithValue("Name", TextBox1.Text);
+            cmd.Parameters.AddWithValue("Password", TextBox2.Text);
+            cmd.Parameters.AddWithValue("City", DropDownList1.SelectedValue);
+            cmd.Parameters.AddWithValue("Gender", RadioButtonList1.SelectedValue);
+            cmd.Parameters.AddWithValue("Mail", TextBox4.Text);
+            cmd.ExecuteNonQuery();
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            DropDownList1.SelectedValue = "";
+            RadioButtonList1.SelectedValue = "";
+            TextBox4.Text = "";
+            TextBox1.Focus();
+
+            con.Close();
+
+           
+
+        }
+        
+    }
+}
